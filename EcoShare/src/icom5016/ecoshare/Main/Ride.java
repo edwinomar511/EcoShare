7522e307c711a9a5ad2bbcab8bd541f22623a58a
edwinomar511 <edwin.ortiz8@upr.edu>
2014-11-01 00:25:00 -0400

    Entity classes modified

--- EcoShare/src/icom5016/ecoshare/Main/Ride.java
+++ EcoShare/src/icom5016/ecoshare/Main/Ride.java
@@ -2,50 +2,73 @@ package icom5016.ecoshare.Main;
 
 public class Ride {
 	
+	private String rideID;
 	private String from;
 	private String to;
 	private double price;
 	private String time;
 	private String date;
+	
 	public Ride(String from, String to, double price, String time, String date) {
-		super();
 		this.from = from;
 		this.to = to;
 		this.price = price;
 		this.time = time;
 		this.date = date;
 	}
+	
 	public String getFrom() {
 		return from;
 	}
+	
 	public void setFrom(String from) {
 		this.from = from;
 	}
+	
 	public String getTo() {
 		return to;
 	}
+	
 	public void setTo(String to) {
 		this.to = to;
 	}
+	
 	public double getPrice() {
 		return price;
 	}
+	
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
 	public String getTime() {
 		return time;
 	}
+	
 	public void setTime(String time) {
 		this.time = time;
 	}
+	
 	public String getDate() {
 		return date;
 	}
+	
 	public void setDate(String date) {
 		this.date = date;
 	}
-	
-	
 
+	/**
+	 * @return the rideID
+	 */
+	public String getRideID() {
+		return rideID;
+	}
+
+	/**
+	 * @param rideID the rideID to set
+	 */
+	public void setRideID(String rideID) {
+		this.rideID = rideID;
+	}
+	
 }
--- EcoShare/src/icom5016/ecoshare/Main/User.java
+++ EcoShare/src/icom5016/ecoshare/Main/User.java
@@ -1,5 +1,10 @@
 package icom5016.ecoshare.Main;
 
 public class User {
+	
+	private String userID;
+	private String name;
+	private String email;
+	private int telephone;
 
 }
