15fd85463e0a122589d24fdab1da7095feefc77e
edwinomar511 <edwin.ortiz8@upr.edu>
2014-11-02 18:34:51 -0400

    Some classes removed. QueryManager more elaborated. Still missing a lot.

--- EcoShare/src/icom5016/ecoshare/Main/QueryManager.java
+++ EcoShare/src/icom5016/ecoshare/Main/QueryManager.java
@@ -1,71 +1,49 @@
 package icom5016.ecoshare.Main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
-
-
+import java.sql.*;
 
 public class QueryManager {
 
 	private Connection connect = null;
 	private Statement statement = null;
-	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
 
 	public QueryManager(){
-		//constructor
-	}
-
-	public boolean findRide(String from, String to, String date, String time, String price){
-		return false;
-	}
-
-	public void readDataBase() throws Exception {
 		try {
-			// this will load the MySQL driver, each DB has its own driver
 			Class.forName("com.mysql.jdbc.Driver");
-			// setup the connection with the DB.
-			connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback?" + "user=sqluser&password=sqluserpw");
+			connect = DriverManager.getConnection("jdbc:mysql://EcoShare" + "user=root&password=t12uprmicom");
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-			// statements allow to issue SQL queries to the database
+	public ResultSet findRide(String query) throws Exception{	 
+		try {
 			statement = connect.createStatement();
-			// resultSet gets the result of the SQL query
-			resultSet = statement.executeQuery("select * from FEEDBACK.COMMENTS");
-			writeResultSet(resultSet);
-
-			// preparedStatements can use variables and are more efficient
-			preparedStatement = connect.prepareStatement("insert into  FEEDBACK.COMMENTS values (default, ?, ?, ?, ? , ?, ?)");
-			// "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-			// parameters start with 1
-			preparedStatement.setString(1, "Test");
-			preparedStatement.setString(2, "TestEmail");
-			preparedStatement.setString(3, "TestWebpage");
-			preparedStatement.setString(5, "TestSummary");
-			preparedStatement.setString(6, "TestComment");
-			preparedStatement.executeUpdate();
-
-			preparedStatement = connect.prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-			resultSet = preparedStatement.executeQuery();
-			writeResultSet(resultSet);
-
-			// remove again the insert comment
-			preparedStatement = connect.prepareStatement("delete from FEEDBACK.COMMENTS where myuser= ? ; ");
-			preparedStatement.setString(1, "Test");
-			preparedStatement.executeUpdate();
-
-			resultSet = statement.executeQuery("select * from FEEDBACK.COMMENTS");
-			writeMetaData(resultSet);
+			resultSet = statement.executeQuery(query); 
 
 		} catch (Exception e) {
 			throw e;
-		} finally {
-			close();
 		}
+		return resultSet;
+	}
+	
+	public boolean addRide(String[] ride){
+		return false;	
+	}
 
+	public boolean removeRide(String rideID){
+		return false;
+	}
+	
+	public boolean addUser(String[] user){
+		return false;
+		
+	}
+	
+	public boolean removeUser(String userID){
+		return false;
+		
 	}
 }
--- EcoShare/src/icom5016/ecoshare/Main/Ride.java
+++ EcoShare/src/icom5016/ecoshare/Main/Ride.java
@@ -1,58 +1,62 @@
 package icom5016.ecoshare.Main;
 
 public class Ride {
-	
 	private String rideID;
 	private String from;
 	private String to;
 	private double price;
 	private String time;
 	private String date;
-	
-	public Ride(String from, String to, double price, String time, String date) {
+	private String comment;
+
+	public Ride(String rideID, String from, String to, double price, String time, String date, String comment) {
+		this.rideID = rideID;
 		this.from = from;
 		this.to = to;
 		this.price = price;
 		this.time = time;
 		this.date = date;
+		this.comment = comment;
 	}
-	
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 	public String getFrom() {
 		return from;
 	}
-	
 	public void setFrom(String from) {
 		this.from = from;
 	}
-	
 	public String getTo() {
 		return to;
 	}
-	
 	public void setTo(String to) {
 		this.to = to;
 	}
-	
 	public double getPrice() {
 		return price;
 	}
-	
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
 	public String getTime() {
 		return time;
 	}
-	
 	public void setTime(String time) {
 		this.time = time;
 	}
-	
 	public String getDate() {
 		return date;
 	}
-	
 	public void setDate(String date) {
 		this.date = date;
 	}
@@ -70,5 +74,5 @@ public class Ride {
 	public void setRideID(String rideID) {
 		this.rideID = rideID;
 	}
-	
 }
+
