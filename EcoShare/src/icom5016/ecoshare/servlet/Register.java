89aa5bbae80b478d59507970205a646d2e219169
Victor <victor.rodriguez26@upr.edu>
2014-11-05 20:03:50 -0400

    Register Queries Done.

--- EcoShare/src/icom5016/ecoshare/servlet/Register.java
+++ EcoShare/src/icom5016/ecoshare/servlet/Register.java
@@ -41,39 +41,48 @@ public class Register extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*String from = request.getParameter("from");
-		String to = request.getParameter("to");
-		Time time = Time.valueOf(request.getParameter("time"));
-		Date date = Date.valueOf(request.getParameter("date"));
-		String price = request.getParameter("price");
 		
-		String query = "SELECT * FROM Ride WHERE Ride.from_location = " + from + "";
-		
-		ResultSet result = qm.findRide(query);
-		
-		if(result.equals(null)){
-			//do something here
-		}
-		else{
-			//do something here too haha...
-		}*/
 		String email= request.getParameter("email");
 		String name = request.getParameter("name");
 		String phone = request.getParameter("phone");
 		String carMake = request.getParameter("carMake");
 		String carModel = request.getParameter("carModel");
 		String carYear = request.getParameter("carYear");
-		System.out.println("doPost method of Register Servlet.");
-		System.out.println(email);
-		System.out.println(name);
-		System.out.println(phone);
-		System.out.println(carMake);
-		System.out.println(carModel);
-		System.out.println(carYear);
+		
+		String user_id = generateUserID(name,phone);
+		
+		String query1 = "INSERT into User (user_id, name, email, telephone)"
+				+ " VALUES ('" + user_id +"', '"+name+"', '"+email+"', "+phone+");";
+		qm.updateQuery(query1);
+		
+		String car_id = generateCarID(carMake,carModel,carYear);
+		String query2 = "INSERT into Car (car_id, carMake, carModel, carYear)"
+				+ " VALUES ('" + car_id +"', '"+carMake+"', '"+carModel+"', "+carYear+");";
+		qm.updateQuery(query2);
+		
+		String query3 =  "INSERT into hasCar (user_id, car_id)"
+				+ " VALUES ('" + user_id +"', '"+car_id+"', '"+");";
+		qm.updateQuery(query3);
 		
 		request.getRequestDispatcher("/RideInformation.jsp").forward(request, response);	
 		
 		
+	}
+	
+	private String generateUserID(String name, String phone){
+		
+		String user_id = (name.substring(0,2))+(phone.substring(3,9));
+		return user_id;
+
+
+	}
+	
+private String generateCarID(String carMake, String carModel, String carYear){
+		
+		String car_id = (carMake.substring(0,4))+(carModel.substring(0,2))+(carYear);
+		return car_id;
+
+
 	}
 
 }
